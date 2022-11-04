cc51a27d649fed59f11438d8fd86d79bac1ea17e
AapoTuulentie <aapo.tuulentie@gmail.com>
2022-11-04 13:33:03 +0200

    NHL

---
+++ viikko1/NHL-tilastot/src/tests/statistics_test.py
@@ -0,0 +1,32 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_player_search(self):
+        player = self.statistics.search("Semenko").name
+
+        self.assertEqual(player, "Semenko")
+
+    def test_team_search(self):
+        team = self.statistics.team("PIT").name
+
+        self.assertEqual(team, "Lemieux")
+
+    
