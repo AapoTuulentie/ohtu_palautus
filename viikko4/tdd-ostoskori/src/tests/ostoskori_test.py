a3af631c90e3dac5285d7aaa67858ff8fc95aeea
AapoTuulentie <aapo.tuulentie@gmail.com>
2022-11-23 18:25:08 +0200

    korissa 2 tuotetta passed

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,3 +21,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tuotetta(self):
+        maito = Tuote("Maito", 3)
+        juusto = Tuote("Juusto", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(juusto)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
