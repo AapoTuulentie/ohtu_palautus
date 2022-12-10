579d35640d8258b43b7902b2df8f0a2978368a7e
AapoTuulentie <aapo.tuulentie@gmail.com>
2022-12-10 16:21:12 +0200

    viikko 6

---
+++ viikko6/query-language/src/builder.py
@@ -0,0 +1,22 @@
+from matchers import *
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self._matcher = matcher
+
+    def build(self):
+        return self._matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+
+    def hasFewerThan(self, value, attribute):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attribute)))
+    
+    def hasAtLeast(self, value, attribute):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attribute)))
+    
+    def oneOf(self, matcher1, matcher2):
+        return QueryBuilder(Or(matcher1, matcher2))
+
+    
